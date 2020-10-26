623d3e5fb771ba2a24a97e3b43b14c71049058bb
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-10-26 12:42:00 +0600

    login function defined

---
+++ emp_db_helper.py
@@ -0,0 +1,33 @@
+import sqlite3
+import datetime
+
+
+def insert():
+    conn = sqlite3.connect('hawk_eye')
+    cur = conn.cursor()
+    cur.execute("insert into employee (name,username, email,password,last_excess,creation_time) values (:name, :username, :email, :password, :last_excess, :creation_time)",
+                {
+                    'name': 'Shima Akter',
+                    'username': 'shima123',
+                    'email': 'shima@gmail.com',
+                    'password': 'qwert',
+                    'last_excess': str(datetime.datetime.now()),
+                    'creation_time': str(datetime.datetime.now())
+
+                }
+                )
+    conn.commit()
+    conn.close()
+
+
+def get_all():
+    conn = sqlite3.connect('hawk_eye')
+    cur = conn.cursor()
+    cur.execute('select * from employee')
+    for item in cur:
+        print(item)
+
+
+insert()
+get_all()
+print(datetime.datetime.now())
