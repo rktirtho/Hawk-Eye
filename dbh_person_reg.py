3a8cb820124b0fb2415a18c1a42e963921015aef
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-11-01 20:13:28 +0600

    person db class function defined

---
+++ dbh_person_reg.py
@@ -0,0 +1,20 @@
+class PersonDBHelper:
+
+    def insert(self):
+        print("inserted")
+
+    def update(self):
+        print("inserted")
+
+    def getAll(self):
+        print("GetAll")
+
+    def getOne(self):
+        print("inserted")
+
+    def getById(self, id):
+        print(id)
+
+    def getByTime(self, time):
+        print(time)
+
