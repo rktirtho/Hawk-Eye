c82c0bdd62431e9b8cb756c39db45ce950aba5bd
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-11-02 10:08:53 +0600

    dashboard page employee insert done

--- dashboard.py
+++ dashboard.py
@@ -4,6 +4,10 @@
 # Actually this UI handle all kinds of Data
 from tkinter import *
 
+LARGEFONT = ("Verdana", 30)
+MEDIAMFONT = ("Verdana", 20)
+SMALLFONT = ("Verdana", 16)
+
 
 root = Tk()
 def click(self):
@@ -12,13 +16,13 @@ root.minsize(900,500)
 root.title("Dashboard")
 # Felt Menu sidebar
 menu_panel = PanedWindow(bg="gray")
-menu_panel.pack(fill=BOTH, expand=1)
+menu_panel.pack(fill=BOTH, expand=2)
 # Menu panel content
 left_label = Frame(menu_panel)
 menu_panel.add(left_label)
 # Add Person
 btn_add_per = Button(left_label, text="Add New Person", command=click)
-btn_add_per.pack(pady=10, padx=20)
+btn_add_per.pack()
 
 btn_add_per2 = Button(left_label, text="Add New Person")
 btn_add_per2.pack()
@@ -27,7 +31,29 @@ btn_add_per2.pack()
 content_panel= PanedWindow(menu_panel, orient=HORIZONTAL)
 menu_panel.add(content_panel)
 
-top = Label(content_panel, text="Top Panner")
-content_panel.add(top)
+contend = Frame(content_panel,)
+title = Label(contend, text = "Add Person", font=LARGEFONT)
+title.pack(pady=20)
+
+lb_emp_id = Label(contend, text="Employee ID")
+lb_emp_id.pack()
+ent_em_id = Entry(contend,)
+ent_em_id.pack()
+
+lb_name = Label(contend, text="Employee Name*")
+lb_name.pack()
+ent_name = Entry(contend,)
+ent_name.pack()
+
+lb_org = Label(contend, text="Organization ID*")
+lb_org.pack()
+ent_org_id = Entry(contend,)
+ent_org_id.pack()
+
+Button(contend, text="Load Image*").pack(pady=10)
+Button(contend, text="Save").pack()
+
+
+content_panel.add(contend)
 
 root.mainloop()
--- dbh_person_reg.py
+++ dbh_person_reg.py
@@ -1,9 +1,22 @@
 from cl_Person import Person
+import sqlite3
+import datetime
 
 
 class PersonDBHelper:
 
-    def insert(self, person):
+    def insert(person):
+        conn = sqlite3.connect('hawk_eye')
+        cur = conn.cursor()
+        cur.execute('INSERT INTO reg_person(emp_id, name, org_id, regester_time) values (:emp_id, :name, :org_id, :regester_time)',
+                    {
+                        'emp_id' : person.get_id(),
+                        'name' : person.get_name(),
+                        'org_id' : person.get_org_id(),
+                        'regester_time' : datetime.datetime.now()
+                    })
+        conn.commit()
+        conn.close()
 
         print("inserted")
 
@@ -11,7 +24,13 @@ class PersonDBHelper:
         print("inserted")
 
     def getAll(self):
-        print("GetAll")
+        conn = sqlite3.connect('hawk_eye')
+        cur = conn.cursor()
+        cur.execute("select * from reg_person")
+        regs = cur.fetchall()
+        for p in regs:
+            print(p)
+        conn.close()
 
     def getOne(self):
         print("inserted")
@@ -22,3 +41,6 @@ class PersonDBHelper:
     def getByTime(self, time):
         print(time)
 
+
+myP = Person(951, "Rejaul Karim", "AA-01")
+PersonDBHelper.getAll(self=None);
